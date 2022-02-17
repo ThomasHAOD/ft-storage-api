15406c55be04c93b7ad51b770c056c0301145f4f
Tam O'Donnell <thomashodonnell@gmail.com>
2022-02-17 10:29:29 +0000

    update circleci config, make api spec generation better

--- src/generate-open-api-spec.ts
+++ src/generate-open-api-spec.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
+import * as YAML from 'yaml';
 
 async function generateOpenApiSpec() {
   const app = await NestFactory.create(AppModule);
@@ -11,8 +12,10 @@ async function generateOpenApiSpec() {
     .setDescription('An API for interacting with Google Cloud Storage')
     .setVersion('1.0')
     .build();
-  const document = JSON.stringify(SwaggerModule.createDocument(app, config));
+  const json = SwaggerModule.createDocument(app, config);
+  const doc = new YAML.Document();
+  doc.contents = json;
 
-  fs.writeFileSync('openApi3Spec.json', document);
+  fs.writeFileSync('openApi2Spec.yaml', doc.toString());
 }
 generateOpenApiSpec();
